b5379c80b434811b1d8e1a67ee8f1aa97a1af586
Shashidhar-Gopal <shashidharg01@gmail.com>
2018-07-10 13:12:31 +0530

    Precompile assets

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_09_173006) do
+ActiveRecord::Schema.define(version: 2018_07_09_170708) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "link_id"
