41c20c70ca98aa232965b1a4a59bc8453ed0f61d
SHASHIDHARA GOPAL <shashidharg01@gmail.com>
2018-07-12 15:47:40 +0530

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,8 +28,8 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   #config.serve_static_assets = false
-  config.assets.js_compressor = :uglifier
-  config.assets.compile = false
+  #config.assets.js_compressor = :uglifier
+  config.assets.compile = true
   #config.assets.digest = true
   #config.assets.debug=true
 
