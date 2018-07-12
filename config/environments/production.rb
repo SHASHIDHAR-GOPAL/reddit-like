232f94de715fa6a1d3911df71d803afb73852a82
Shashidhar-Gopal <shashidharg01@gmail.com>
2018-07-12 15:12:18 +0530

    Change production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.serve_static_assets = false
+  #config.serve_static_assets = false
   config.assets.js_compressor = :uglifier
   config.assets.compile = false
   config.assets.digest = true
